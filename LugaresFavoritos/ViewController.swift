853f295c6d9eae8fcbe8de41ae6795bcf24d35ed
paulomatos <pauloricardomattos@gmail.com>
2016-05-08 14:52:12 -0300

    Update
    First update

--- LugaresFavoritos/ViewController.swift
+++ LugaresFavoritos/ViewController.swift
@@ -7,12 +7,103 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var map: MKMapView!
+    
+    var manager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 3.0
+        
+        map.addGestureRecognizer(uilpgr)
+    }
+    
+    
+    func action(gestureRecognizer: UIGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+            let touchPoint = gestureRecognizer.locationInView(self.map)
+            let newCoordinate = self.map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placeMarks, myError) in
+                var title = ""
+                
+                if (myError == nil) {
+                    
+                    if let p = placeMarks?[0] {
+                        
+                        var address: String = ""
+                        var address2: String = ""
+                        
+                        if p.subThoroughfare != nil {
+                            
+                            address2 = p.subThoroughfare!
+                        }
+                        
+                        if p.thoroughfare != nil {
+                            
+                            address = p.thoroughfare!
+                        }
+                        
+                        title = "\(address2) \(address)"
+                        
+                    }
+                    
+                }
+                
+                // retira espaço em branco antes de depois das words
+                if title.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) == "" {
+                    
+                    title = "Adicionado em \(NSDate())"
+                    
+                }
+                
+                places.append(["name": title, "late":"\(newCoordinate.latitude)", "lon":"\(newCoordinate.longitude)" ])
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                
+                self.map.addAnnotation(annotation)
+            })
+            
+            
+        }
+        
+    }
+    
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        let latitute = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let coordinate = CLLocationCoordinate2DMake(latitute, longitude)
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        self.map.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
